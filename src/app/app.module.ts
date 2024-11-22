ad5a84050c93d3174d1641206dbc6b99f14d9dbd
LennynT219 <lennynvega35@gmail.com>
2024-11-22 15:34:19 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({ "projectId": "clases-6", "appId": "1:496521316193:web:38d3724fa03201e92e6720", "storageBucket": "clases-6.firebasestorage.app", "apiKey": "AIzaSyCu77EaUsMBBM5wCUCc7MGdVJEgUiQsrwc", "authDomain": "clases-6.firebaseapp.com", "messagingSenderId": "496521316193", "measurementId": "G-WCJSMF326R" })), provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"clases-6","appId":"1:496521316193:web:38d3724fa03201e92e6720","storageBucket":"clases-6.firebasestorage.app","apiKey":"AIzaSyCu77EaUsMBBM5wCUCc7MGdVJEgUiQsrwc","authDomain":"clases-6.firebaseapp.com","messagingSenderId":"496521316193","measurementId":"G-WCJSMF326R"}))],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
