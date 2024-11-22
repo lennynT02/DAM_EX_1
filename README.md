c549c6b71fce21a1646b39309594713ca5a83f75
LennynT219 <lennynvega35@gmail.com>
2024-11-22 16:01:58 -0500

    add: APK

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 Examen 1 de Desarrollo de aplicaciones Moviles
 
 ## Captura del Index
+
 ![Captura del Index](./capturas/index.png)
 
 ## Captura de FireBase
+
 ![Captura de FireBase](./capturas/fire.png)
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,23 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Examen',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'FIT_CENTER',
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
 
 export default config;
+
